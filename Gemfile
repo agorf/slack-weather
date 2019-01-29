cf202efd17d2ba7528155239413e0140751ea9cf
Angelos Orfanakos <me@agorf.gr>
2019-01-29 20:35:51 +0200

    Remove dotenv Gem
    It's just one environment variable that you have to define.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'dotenv', '~> 2.4.0'
 gem 'nokogiri', '~> 1.8.5'
--- slack_poster.rb
+++ slack_poster.rb
@@ -1,5 +1,4 @@
 require_relative 'message_composer'
-require 'dotenv/load'
 require 'json'
 require 'net/http'
 require 'uri'
